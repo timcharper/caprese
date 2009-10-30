65282544a5998dc85866208d12c38ae14cf9393c
Tim Harper <timcharper@gmail.com>
2009-10-29 18:06:56 -0600

    fixes Caprese issue: hangs while trying to growl when growl is not running

--- lib/growl_helper.rb
+++ lib/growl_helper.rb
@@ -3,6 +3,7 @@ module GrowlHelper
   extend self
   
   def growl(title, message)
+    return unless Application.new("GrowlHelperApp").running?
     app("GrowlHelperApp").register(:all_notifications => [title], :as_application => APP_NAME, :icon_of_application => "Pomodoro", :default_notifications => [title])
     app("GrowlHelperApp").notify(:title => title, :application_name => APP_NAME, :with_name => title, :description => message)
   end
