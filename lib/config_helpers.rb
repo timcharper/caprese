06772057689febfc7fd41a7ef9a7d45ae30cf392
Tim Harper <timcharper@gmail.com>
2010-03-26 10:56:34 -0600

    there is no reason we need such long environment variable names

--- lib/config_helpers.rb
+++ lib/config_helpers.rb
@@ -4,10 +4,11 @@ module ConfigHelpers
   end
 
   def stop_time
-    (Time.now + (ENV['POMODORO_DURATION'] || 25).to_i * 60)
+    (Time.now + (ENV['DURATION'] || ENV['POMODORO_DURATION'] || 25).to_i * 60)
   end
 
+  attr_writer :description
   def description
-    ENV["POMODORO_DESCRIPTION"]
+    @description ||= (ENV["DESCRIPTION"] || ENV["POMODORO_DESCRIPTION"]).to_s.dup
   end
 end
