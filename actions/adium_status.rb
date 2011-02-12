a0bfd506ce468ac4bfb77153a1e766942c091d22
Tim Harper <timcharper@gmail.com>
2011-02-12 14:41:26 -0700

    don't try to set Adium status unless it's running
    (don't launch Adium just so you can change the status)

--- actions/adium_status.rb
+++ actions/adium_status.rb
@@ -4,16 +4,22 @@ class AdiumStatus < CapreseAction
   config_schema({:away => String, :available => String})
 
   def start
+    return unless running?
     adium_accounts.status_type.set(:away)
     adium_accounts.status_message.set(config[:away])
   end
   
   def stop
+    return unless running?
     adium_accounts.status_type.set(:available)
     adium_accounts.status_message.set(config[:available])
   end
   
   def adium_accounts
     @adium_accounts ||= app("Adium").accounts
+  end
+
+  def running?
+     app("System Events").processes["Adium"].exists
   end
 end
